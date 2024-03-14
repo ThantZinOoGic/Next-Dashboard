4caa27edcf86dabdd3f4d41686e26cb51b43da02
ThantZinOoGic <thantzinoo@gicjp.com>
2024-03-14 14:40:20 +0630

    after UI validation

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -20,6 +20,8 @@ export default async function Page({
 
   const totalPages = await fetchInvoicesPages(query);
 
+ 
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
